28ce27bbb67eef3586ccb471c30ee4edc8acd181
khwilo <khwilowatai@gmail.com>
2019-04-05 13:08:02 +0300

    Add email prop

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Inbox from './components/Inbox';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const email = {
+    sender : "React Newsletter",
+    subject: "React Newsletter - Issue 36",
+    date   : "Jul 15",
+    message: "React Newsletter Issue 36 - July 15th 2016 Read this issue on the web http://reactjsnewsletter.com/issues/36?sid=3QGDYBX ### Comme"
+};
+
+ReactDOM.render(<Inbox email={email} />, document.getElementById('root'));
